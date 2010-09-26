267c67dd57d2df5a1e08173772c12598e4892b1e
cwoodruf@gmail.com <cwoodruf@gmail.com@a200258b-fe9b-fc60-071b-1a6adaaf62b4>
2010-09-26 06:25:08 +0000

    cleanup

--- perl/test.pl
+++ perl/test.pl
@@ -1,8 +1,6 @@
 #!/usr/bin/perl
-use CGI qw/header start_html end_html/;
-use strict;
-
-print header,start_html("test of perl");
-print join "<br>\n", map { "$ENV{$_} = $_" } sort keys %ENV;
+use CGI qw/:standard/;
+our $count++;
+print header,start_html("test of perl $count"),h3("Perl CGI $count");
+print join "<br>\n", map { "$_ = $ENV{$_}" } reverse sort keys %ENV;
 print end_html;
-
