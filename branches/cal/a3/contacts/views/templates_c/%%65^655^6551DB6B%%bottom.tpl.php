03445cd995ee6082d604a5410e1fba7f70d97957
cwoodruf@gmail.com <cwoodruf@gmail.com@a200258b-fe9b-fc60-071b-1a6adaaf62b4>
2010-10-13 08:00:14 +0000

    removed temporary compiled templates

--- branches/cal/a3/contacts/views/templates_c/%%65^655^6551DB6B%%bottom.tpl.php
+++
@@ -1,6 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2010-10-12 18:25:59
-         compiled from bottom.tpl */ ?>
-</div>
-
-</body>
-</html>
--- branches/cal/a3/contacts/views/templates_c/%%BC^BC5^BC599B42%%pwlink.tpl.php
+++
@@ -1,3 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2010-10-12 20:12:52
-         compiled from pwlink.tpl */ ?>
-<a href="index.php?action=home/user/changepassword">Change password</a>
