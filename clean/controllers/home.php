d25beabbbe36290d8a2b36aad372a391f2135892
cwoodruf@gmail.com <cwoodruf@gmail.com@a200258b-fe9b-fc60-071b-1a6adaaf62b4>
2010-10-25 00:29:32 +0000

    default home controller

---
+++ clean/controllers/home.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * default controller - customize it
+ *
+ * you can use the $this->actions array to select which action you want to do
+ * use $this->doable(array( ...map of action to callback funcs...));
+ * and $this->doaction($this->actions[1]);
+ * to handle sub actions such as home/someaction
+ *
+ * you can display templates with the default smarty view code 
+ * View::assign - to assign template variables
+ * View::display - display the template
+ * View::wrap - display template in a wrapper 
+ */
+class Home extends Controller {
+	/**
+	 * required function for controllers
+	 */
+	public function execute() {
+		//todo get me to do something
+		print "<h1>hello</h1>";
+	}
+}
+
