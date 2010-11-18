5d45a3fd2a96af2cd3f729621c47a1afa6ea1136
cwoodruf@gmail.com <cwoodruf@gmail.com@a200258b-fe9b-fc60-071b-1a6adaaf62b4>
2010-11-18 05:45:39 +0000

    -Added new framework to v1 folder

---
+++ v1/lib/login.php
@@ -0,0 +1,112 @@
+<?php
+session_start();
+
+/** 
+ * class to handle logging in, to use it make a table to hold login data
+ * and build a model with db/buildmodels.php for the table
+ * the class should implement the PW interface in lib/pw.php
+ * by default this is whatever LOGINCLASS is
+ */
+class Login {
+
+	private static $ldata;
+	private static $errors;
+
+	public static function logout() {
+		self::$ldata = null;
+		unset($_SESSION[LOGINSESSION]);
+		unset($_COOKIE['from']);
+		setcookie('from',null,mktime(0,0,0,1,1,1970));
+	}
+
+	public static function check() {
+		$pwclass = LOGINMODEL;
+		return self::authenticate($pwclass);
+	}
+
+	private static function authenticate($pwclass) {
+		# if already logged in then 
+		if (isset($_SESSION[LOGINSESSION]) and is_array($_SESSION[LOGINSESSION])) 
+			return $_SESSION[LOGINSESSION];
+
+		$pw = new $pwclass;
+		$login = isset($_REQUEST['login']) ? $_REQUEST['login'] : '';
+		if (!$pw->valid_login($login)) return;
+
+		$password = $_REQUEST['password'];
+
+		if (!$pw->valid_pw($password)) return;
+		$password = $pw->encode_pw($password);
+
+		$ldata = $pw->get_login($login);
+		$loginok = ($ldata['password'] === $password) ? true : false;
+
+		if ($loginok) {
+			$ldata = self::save_login($login,$ldata);
+			return $ldata;
+		}
+		return;
+	}
+
+	public static function encode($pw) {
+		require(SALTFILE);
+		return sha1($pw.SALT);
+	}
+
+	/**
+	 * convenience method for checking the password data in 
+	 * tools/passwordform.tpl
+	 */
+	public static function getpw($pwclass=LOGINMODEL) {
+		self::err(null,true);
+		$ldata = Login::authenticate($pwclass);
+		if (!is_array($ldata)) {
+			self::err("you are not logged in!");
+			return false;
+		}
+		$pw = new $pwclass;
+		$me = $pw->getone($ldata['login']);
+		if ($me['password'] != $pw->encode_pw($_REQUEST['old_password'])) {
+			self::err("old password was wrong!");
+		}
+		if (!Check::validpassword($newpw=$_REQUEST['new_password'])) {
+			self::err("invalid password ".Check::err());
+		}
+		if ($newpw == $_REQUEST['old_password']) {
+			self::err("new password same as old password - aborting");
+			return false;
+		}
+		if ($newpw != $_REQUEST['confirm_password']) {
+			self::err("new passwords don't match!");
+		}
+		if (count(self::err())) {
+			return false;
+		}
+		return $pw->encode_pw($newpw);
+	}
+	
+	public static function save_login($this_login,$ldata) {
+		unset($ldata['password']);
+		self::$ldata = $ldata;
+		$_SESSION[LOGINSESSION] = $ldata;
+		$_SESSION[LOGINSESSION]['login'] = $this_login;
+		$_SESSION[LOGINSESSION]['time'] = $time = time();
+		return $_SESSION[LOGINSESSION];
+	}
+
+	public static function refresh() {
+		$ldata = self::check();
+		if (!$ldata) return;
+		$newldata = Run::me(LOGINMODEL,'get_login',$ldata['login']);
+		if ($newldata) {
+			Login::save_login($ldata['login'], $newldata);
+		}
+		return $newldata;
+	}
+	
+	public static function err($error=null,$refresh=false) {
+		if ($refresh) self::$errors = array();
+		if (isset($error)) self::$errors[] = $error;
+		return self::$errors;
+	}
+}
---
+++ v1/models/base/v1-schema.php
@@ -0,0 +1,69 @@
+<?php
+$schema = array();
+
+$dbhost = 'localhost';
+$dbname = 'v1';
+
+$schema['Admin'] = array(
+	'adminID' => array( 'type' => 'int', 'size' => 11, 'key' => true, 'auto' => 1, ),
+	'permissions' => array( 'type' => 'varchar', 'size' => 32, ),
+	'created' => array( 'type' => 'timestamp', ),
+);
+
+$schema['Job'] = array(
+	'jobID' => array( 'type' => 'int', 'size' => 11, 'key' => true, 'auto' => 1, ),
+	'organizationID' => array( 'type' => 'int', 'size' => 11, ),
+	'search_text' => array( 'type' => 'varchar', 'size' => 255, ),
+	'title' => array( 'type' => 'varchar', 'size' => 64, ),
+	'time' => array( 'type' => 'varchar', 'size' => 20, ),
+	'street_address' => array( 'type' => 'varchar', 'size' => 255, ),
+	'city' => array( 'type' => 'varchar', 'size' => 64, ),
+	'country' => array( 'type' => 'varchar', 'size' => 32, ),
+	'description' => array( 'type' => 'text', ),
+	'url' => array( 'type' => 'varchar', 'size' => 128, ),
+	'visibility_status' => array( 'type' => 'varchar', 'size' => 32, ),
+	'requirements' => array( 'type' => 'text', ),
+	'created' => array( 'type' => 'timestamp', ),
+);
+
+$schema['Login'] = array(
+	'email' => array( 'type' => 'varchar', 'size' => 128, 'key' => true, ),
+	'password' => array( 'type' => 'varchar', 'size' => 64, ),
+	'user_type' => array( 'type' => 'varchar', 'size' => 64, ),
+	'external_key' => array( 'type' => 'int', 'size' => 11, ),
+	'created' => array( 'type' => 'timestamp', ),
+);
+
+$schema['Organization'] = array(
+	'organizationID' => array( 'type' => 'int', 'size' => 11, 'key' => true, 'auto' => 1, ),
+	'name' => array( 'type' => 'varchar', 'size' => 128, ),
+	'address' => array( 'type' => 'text', ),
+	'contact_name_first' => array( 'type' => 'varchar', 'size' => 64, ),
+	'contact_name_last' => array( 'type' => 'varchar', 'size' => 64, ),
+	'contact_phone' => array( 'type' => 'varchar', 'size' => 32, ),
+	'contact_email' => array( 'type' => 'varchar', 'size' => 128, ),
+	'url' => array( 'type' => 'varchar', 'size' => 128, ),
+	'visibility_status' => array( 'type' => 'varchar', 'size' => 32, ),
+	'description' => array( 'type' => 'text', ),
+	'created' => array( 'type' => 'timestamp', ),
+);
+
+$schema['Schedule'] = array(
+	'PRIMARY KEY' => array('jobID' => '', 'volunteerID' => '', 'start' => '', ),
+	'jobID' => array( 'type' => 'int', 'size' => 11, ),
+	'volunteerID' => array( 'type' => 'int', 'size' => 11, ),
+	'job_date' => array( 'type' => 'datetime', 'size' => 20, ),
+	'start' => array( 'type' => 'datetime', ),
+	'hours' => array( 'type' => 'int', 'size' => 11, ),
+	'hours_worked' => array( 'type' => 'int', 'size' => 11, ),
+);
+
+$schema['Volunteer'] = array(
+	'volunteerID' => array( 'type' => 'int', 'size' => 11, 'key' => true, 'auto' => 1, ),
+	'name_first' => array( 'type' => 'varchar', 'size' => 64, ),
+	'name_last' => array( 'type' => 'varchar', 'size' => 63, ),
+	'phone' => array( 'type' => 'varchar', 'size' => 32, ),
+	'email' => array( 'type' => 'varchar', 'size' => 128, ),
+	'created' => array( 'type' => 'timestamp', ),
+);
+
