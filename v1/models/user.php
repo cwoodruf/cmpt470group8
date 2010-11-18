6127c36b6755730e4c56d49a019da4032cf2509c
cwoodruf@gmail.com <cwoodruf@gmail.com@a200258b-fe9b-fc60-071b-1a6adaaf62b4>
2010-11-18 05:43:02 +0000

    --Deleted v1 directory, for updated framework

--- v1/models/user.php
+++
@@ -1,9 +0,0 @@
-<?php
-class User extends LoginEntity{
-	public function __construct() {
-		parent::__construct();
-		// the 'Check' key tells the model to look for a static method in the 'Check' class
-		$this->schema['email']['checker']['Check'] = 'isemail';
-		
-	    }	
-}
--- v1/views/templates/tools/hiddenfields.tpl
+++
@@ -1,5 +0,0 @@
-
-{foreach from=$this->hidden key=field item=value}
-<input type=hidden name="{$field}" value="{$value}">
-{/foreach}
-
