09c119d543cc98beaea7d1527dc4903b3ca217af
cwoodruf@gmail.com <cwoodruf@gmail.com@a200258b-fe9b-fc60-071b-1a6adaaf62b4>
2010-11-28 23:51:28 +0000

    attempted to get logins to show the same page but the javascript history function is not working as expected

--- trunk/v2/controllers/logout.php
+++ trunk/v2/controllers/logout.php
@@ -2,7 +2,8 @@
 class Logout extends Controller {
 	public function execute() {
 		Login::logout();
-		$h = new Home($this->actions);
-		$h->execute();
+		if ($this->actions[1] == 'ajax') return;
+		$c = new Home(array('home'));
+		$c->execute;
 	}
 }
